995d2b03c7f14620a4911ed6b432ebd7ee0f2ae2
Ankush <ankushchalla@gmail.com>
2021-01-31 21:31:29 -0800

    Defining port.

--- Develop/app.js
+++ Develop/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 let app = express();
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
